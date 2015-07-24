5b523757711ce71527cadf4d6c19f714d560ff17
V3499 (Conference ID) <v3499@cam.ac.uk>
2015-07-24 11:24:40 +0100

    test

--- drone-backend.js
+++ drone-backend.js
@@ -2,6 +2,7 @@ var Cylon = require('cylon');
 var ws = require('nodejs-websocket');
 var bot;
 
+
 // Initialise the robot
 Cylon.robot()
     .connection("ardrone", {
@@ -12,11 +13,42 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+connection: "ardrone"
+})
     .on("ready", fly);
-    
-// Fly the bot
-function fly(robot) {
 
+function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+
+    after(2*1000, function() {
+        bot.drone.left(0.5);
+    });
+
+    after(4*1000, function() {
+        bot.drone.hover();
+    });
+
+    after(8*1000, function() {
+        bot.drone.forward(0.5);
+    });
+
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+
+    /*bot.drone.right (0.5);
+    bot.drone.front(0.5);
+    bot.drone.back (0.5);
+    bot.drone.left (0.5);
+
+    bot.drone.up (0.5);
+    bot.drone.down (0.5);
+    bot.drone.land();*/
 }
 
 Cylon.start();
